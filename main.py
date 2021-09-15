44f0c089ce60c7116f976a9b38f6ff7ff0430f9a
Marios1989 <68182435+Marios1989@users.noreply.github.com>
2021-09-15 17:27:53 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,56 @@
+import cv2
+import numpy as np
+
+import warnings
+
+warnings.filterwarnings("ignore")
+
+cap = cv2.VideoCapture('/home/user/Downloads/Flashing traffic lights.avi')
+
+frame_width = int(cap.get(3))
+frame_height = int(cap.get(4))
+
+size = (frame_width, frame_height)
+
+result = cv2.VideoWriter('filename.avi',
+                         cv2.VideoWriter_fourcc(*'MJPG'),
+                         10, size)
+
+
+while True:
+    ret, frame = cap.read()
+
+    if ret == True:
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+        lower_yellow = np.array([20, 0, 0])
+        upper_yellow = np.array([40, 255, 255])
+
+        mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
+        res = cv2.bitwise_and(frame, frame, mask=mask)
+
+        img = cv2.medianBlur(res, 5)
+        ccimg = cv2.cvtColor(img, cv2.COLOR_HSV2BGR)
+        cimg = cv2.cvtColor(ccimg, cv2.COLOR_BGR2GRAY)
+        circles = cv2.HoughCircles(cimg, cv2.HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=20, maxRadius=30)
+        if circles is not None:
+            print("circle is found")
+            circles = np.uint16(np.around(circles))
+            for i in circles[0, :]:
+                cv2.circle(cimg, (i[0], i[1]), i[2], (0, 255, 0), 2)
+                cv2.circle(cimg, (i[0], i[1]), 2, (0, 0, 255), 3)
+        cv2.imshow('detected circles', cimg)
+        cv2.imshow('res', res)
+
+        result.write(frame)
+
+        cv2.imshow('Frame', frame)
+
+    else:
+        print("Read Failed")
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+cap.release()
+result.release()
+cv2.destroyAllWindows()
